0bf7fd519ea5b2eb3f5918021df86690ae7bdad7
Quinn Davis <Quoncdavis@gmail.com>
2025-03-28 18:06:55 -0600

    add dash to player controller'

--- Player.cs
+++ Player.cs
@@ -3,38 +3,80 @@ using System;
 
 public partial class Player : RigidBody2D
 {
-	[Export] float speed;
-	
-	// helpers
-	private Object RayCast(Vector2 start, Vector2 end){
-		var spaceState = GetWorld2D().DirectSpaceState;
-		var query = PhysicsRayQueryParameters2D.Create(start, end);
-		var result = spaceState.IntersectRay(query);
-		if (result.Count > 0){
-			return result["collider"];
-		}
+    [Export] private float speed;
+    [Export] private int health;
+    [Export] private float hunger;
+    [Export] private float hungerRate;
+    [Export] private int attackDamage;
+    [Export] private Curve dashCurve;
+    [Export] private float dashPower;
+    [Export] private float dashTimeMax;
 
-		return null;
-	}
-	public override void _Ready()
-	{
-		
-	}
+    private Vector2 dashDir;
+    private bool isDashing;
+    private float dashTime;
 
-	public override void _Process(double delta)
-	{
-		Move();
-	}
-	public void _input(InputEvent @event){
-		if (@event.IsActionPressed("hit"))
-		{
-			GD.Print("hit");
-		}
-	}
 
-	void Move()
-	{
-		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
-		LinearVelocity = inputDir * speed;
-	}
+    // helpers
+    private Object RayCast(Vector2 start, Vector2 end)
+    {
+        var spaceState = GetWorld2D().DirectSpaceState;
+        var query = PhysicsRayQueryParameters2D.Create(start, end);
+        var result = spaceState.IntersectRay(query);
+        if (result.Count > 0)
+        {
+            return result["collider"];
+        }
+
+        return null;
+    }
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+       
+        hunger += (float)delta * hungerRate;
+        
+        if (isDashing)
+        {
+            dashTime += (float)delta;
+            if (dashTime > dashTimeMax)
+            {
+                dashTime = dashTimeMax;
+                isDashing = false;
+            }
+
+            float targetVel = dashCurve.Sample(dashTime / dashTimeMax) * dashPower;
+            LinearVelocity = dashDir * targetVel;
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    public void _input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("hit"))
+        {
+            GD.Print("hit");
+        }
+
+        if (@event.IsActionPressed("dash"))
+        {
+            isDashing = true;
+            dashTime = 0;
+            dashDir = GetLocalMousePosition().Normalized();
+            GD.Print(dashDir);
+        }
+
+    }
+
+    void Move()
+    {
+        LinearVelocity = Input.GetVector("left", "right", "up", "down") * speed;
+    }
 }
