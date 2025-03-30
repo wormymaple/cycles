1eaf54d499118bcd80fb846c7c4bb646fe401e9c
Quinn Davis <Quoncdavis@gmail.com>
2025-03-30 16:38:45 -0600

    refactor animation code to work with individual body parts, add eyes and feet to player scene

--- Player.cs
+++ Player.cs
@@ -11,12 +11,33 @@ public partial class Player : RigidBody2D
     [Export] private Curve dashCurve;
     [Export] private float dashPower;
     [Export] private float dashTimeMax;
-	[Export] Camera2D mainCamera;
-	[Export] Sprite2D hand1, hand2;
-	[Export] Vector2 handOffset;
-	[Export] float handWiggleIntensity, handWiggleSpeed;
-	float handWiggleT;
-	[Export] float forceHandPerspCutoff;
+    [Export] Camera2D mainCamera;
+
+    [Export] private Sprite2D torch;
+
+    float wiggleT;
+    [Export] private float wiggleSpeed, wiggleIntensity;
+    [Export] float forcePerspCutoff;
+
+    [Export] Sprite2D hand1, hand2;
+    [Export] Vector2 handOffset;
+    [Export] private Vector2 handVerticalShift;
+
+
+    [Export] private Sprite2D eye1, eye2;
+    [Export] private Vector2 eyeOffset;
+    [Export] private Vector2 eyeVerticalShift;
+
+
+    [Export] private Sprite2D foot1, foot2;
+    [Export] private Vector2 footOffset;
+    [Export] private Vector2 footVerticalShift;
+
+
+    [Export] private Sprite2D mouth;
+    [Export] private Vector2 mouthOffset;
+    [Export] private Vector2 mouthVerticalShift;
+
 
     private Vector2 dashDir;
     private bool isDashing;
@@ -43,9 +64,8 @@ public partial class Player : RigidBody2D
 
     public override void _Process(double delta)
     {
-       
         hunger += (float)delta * hungerRate;
-        
+
         if (isDashing)
         {
             dashTime += (float)delta;
@@ -62,30 +82,45 @@ public partial class Player : RigidBody2D
         {
             Move((float)delta);
         }
-		
-		Animate();
+
+        Animate();
     }
-    
+
+    void AnimateBodyPart(Sprite2D bodyP1, Vector2 lookDir, Vector2 perpDir, Vector2 wiggle, Vector2 offset,
+        Vector2 verticalShift)
+    {
+        if (LinearVelocity == Vector2.Zero) wiggle = Vector2.Zero;
+        bodyP1.Position = perpDir * offset + wiggle + verticalShift;
+        bodyP1.FlipH = lookDir.X > 0;
+        bodyP1.ZIndex = perpDir.Y < 0 ? -1 : 1;
+
+        if (Mathf.Abs(lookDir.X) < forcePerspCutoff)
+        {
+            int order = lookDir.Y < 0 ? -1 : 1;
+            bodyP1.ZIndex = order;
+        }
+    }
+
     void Animate()
     {
-		Vector2 mousePos = mainCamera.GetGlobalMousePosition();
-		Vector2 lookDir = (mousePos - GlobalPosition).Normalized();
+        Vector2 lookDir = GetLocalMousePosition().Normalized();
+        Vector2 perpDir = lookDir.Rotated(Mathf.Pi / 2);
+        Vector2 wiggle = Mathf.Sin(wiggleT * wiggleSpeed) * wiggleIntensity * lookDir;
+        
+        AnimateBodyPart(hand1, lookDir, perpDir, wiggle, handOffset, handVerticalShift);
+        AnimateBodyPart(hand2, lookDir, -perpDir, -wiggle, handOffset, handVerticalShift);
+        
+        AnimateBodyPart(eye1, lookDir, perpDir, wiggle, eyeOffset, eyeVerticalShift);
+        AnimateBodyPart(eye2, lookDir, -perpDir, -wiggle, eyeOffset, eyeVerticalShift);
+        
+        AnimateBodyPart(foot1, lookDir, perpDir, wiggle, footOffset, footVerticalShift);
+        AnimateBodyPart(foot2, lookDir, -perpDir, -wiggle, footOffset, footVerticalShift);
+        
+        AnimateBodyPart(torch, lookDir, -perpDir, -wiggle, handOffset, handVerticalShift);
 
-		Vector2 perpDir = lookDir.Rotated(Mathf.Pi / 2);
-		Vector2 handWiggle = Mathf.Sin(handWiggleT * handWiggleSpeed) * handWiggleIntensity * lookDir;
-		hand1.Position = perpDir * handOffset + handWiggle;
-		hand2.Position = -perpDir * handOffset - handWiggle;
 
-		hand1.ZIndex = perpDir.Y < 0 ? -1 : 1;
-		hand2.ZIndex = -perpDir.Y < 0 ? -1 : 1;
+    }
 
-		if (Mathf.Abs(lookDir.X) < forceHandPerspCutoff)
-		{
-			int order = lookDir.Y < 0 ? -1 : 1;
-			hand1.ZIndex = order;
-			hand2.ZIndex = order;
-		}
-  	}
 
     public void _input(InputEvent @event)
     {
@@ -101,13 +136,12 @@ public partial class Player : RigidBody2D
             dashDir = GetLocalMousePosition().Normalized();
             GD.Print(dashDir);
         }
-
     }
 
     void Move(float delta)
-    {		
-		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
-		handWiggleT += delta * inputDir.Length();
-		LinearVelocity = inputDir * speed;
+    {
+        Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
+        wiggleT += delta * inputDir.Length();
+        LinearVelocity = inputDir * speed;
     }
 }
