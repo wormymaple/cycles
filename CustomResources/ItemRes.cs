e650d1945032007acb88fc5dd48da27b204510b2
Quinn Davis <Quoncdavis@gmail.com>
2025-04-16 23:18:58 -0600

    create object spawning system for chunks, probability based with (# of max items in chunk) (probability out (0.0 -> 1.0) of each possible item spawning)

--- CustomResources/ItemRes.cs
+++ CustomResources/ItemRes.cs
@@ -4,18 +4,35 @@ using System;
 [GlobalClass]
 public partial class ItemRes : Resource
 {
-	[Export] public string Name;
-	[Export] public bool Stackable, Equippable;
-	[Export] public Texture2D InventoryTex;
-	[Export] public PackedScene EquippedScene;
+    [ExportCategory("rules")]
+    [Export] public string Name;
+    [Export] public bool Stackable, Equippable;
+    [Export] public int[] NumsPerDrop; // array of possible qtys in dropped stack
+    
+    [ExportCategory("assets and scene")]
+    [Export] public Texture2D InventoryTex;
 
-	public ItemRes() : this("none", true, false, null, null) {}
-	public ItemRes(string name, bool stackable, bool equippable, Texture2D inventoryTex, PackedScene equippedScene)
-	{
-		Name = name;
-		Stackable = stackable;
-		Equippable = equippable;
-		InventoryTex = inventoryTex;
-		EquippedScene = equippedScene;
-	}
+    [Export] public AudioStream PickupNoise;
+    [Export] public AudioStream FullInvNoise;
+    
+    [Export] public PackedScene EquippedScene;
+    
+    [ExportCategory("spawning stats")]
+    [Export] public float probValue;
+    [Export] public int numSpawns;
+    
+
+
+    public ItemRes() : this("none", true, false, null, null)
+    {
+    }
+
+    public ItemRes(string name, bool stackable, bool equippable, Texture2D inventoryTex, PackedScene equippedScene)
+    {
+        Name = name;
+        Stackable = stackable;
+        Equippable = equippable;
+        InventoryTex = inventoryTex;
+        EquippedScene = equippedScene;
+    }
 }
