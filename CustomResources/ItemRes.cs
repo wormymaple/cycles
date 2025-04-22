50c65ce8f4e66f3fa29acf7103a4fa710918b177
Quinn Davis <Quoncdavis@gmail.com>
2025-04-22 15:30:59 -0600

    refactor - remove spawning data from ItemRes class (now stored in specific spawner nodes)

--- CustomResources/ItemRes.cs
+++ CustomResources/ItemRes.cs
@@ -7,22 +7,12 @@ public partial class ItemRes : Resource
     [ExportCategory("rules")]
     [Export] public string Name;
     [Export] public bool Stackable, Equippable;
-    [Export] public int[] NumsPerDrop; // array of possible qtys in dropped stack
     
     [ExportCategory("assets and scene")]
     [Export] public Texture2D InventoryTex;
-
     [Export] public AudioStream PickupNoise;
     [Export] public AudioStream FullInvNoise;
-    
     [Export] public PackedScene EquippedScene;
-    
-    [ExportCategory("spawning stats")]
-    [Export] public float probValue;
-    [Export] public int numSpawns;
-    
-
-
     public ItemRes() : this("none", true, false, null, null)
     {
     }
